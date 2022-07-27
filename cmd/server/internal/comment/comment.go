0b4df6696fe3ec62036d4d5569bc1d207d0a3430
Ayabonga <ayabongabooi2@gmail.com>
2022-07-27 18:42:42 +0200

    wip(server_layer): [] implement comment server layer

---
+++ cmd/server/internal/comment/comment.go
@@ -0,0 +1,45 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+// comment - a representation of a comment
+// structure for our service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(ctx context.Context, id string) (Comment, error)
+}
+
+// service -  is the struct on which all our
+// logic will be applied/ be build on top of
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new
+// service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("getting comment")
+
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+
+	return cmt, nil
+}
