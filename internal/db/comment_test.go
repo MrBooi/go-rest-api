4f79a7008a5ff082a3cb7f18205f76856739df83
Ayabonga <ayabongabooi2@gmail.com>
2022-09-13 13:25:19 +0200

    test(comment_go): add database comment tests

---
+++ internal/db/comment_test.go
@@ -0,0 +1,81 @@
+//go:build integration
+// +build integration
+
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/MrBooi/go-rest-api/internal/comment"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommentDatabase(t *testing.T) {
+	t.Run("test create comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		newCmt, err := db.GetComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, "slug", newCmt.Slug)
+	})
+
+	t.Run("test delete comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		err = db.DeleteComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+
+		_, err = db.GetComment(context.Background(), cmt.ID)
+		assert.Error(t, err)
+	})
+
+	t.Run("test update comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		cmt.Body = "new-Body"
+		cmt, err = db.UpdateComment(context.Background(), cmt.ID, cmt)
+		assert.NoError(t, err)
+
+		newCmt, err := db.GetComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, "new-Body", newCmt.Body)
+	})
+
+	t.Run("test get comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		newCmt, err := db.GetComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, "slug", newCmt.Slug)
+
+	})
+}
